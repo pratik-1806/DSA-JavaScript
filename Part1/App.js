4ecfae85f86106de8d8dab55fd25038fa471d91b
pratik-1806 <ppratikpansare98@gmail.com>
2025-07-25 22:43:06 +0530

    Swapping using 3 mehods

--- Part1/App.js
+++ Part1/App.js
@@ -1,5 +1,27 @@
-var a=1;
-var b=2;
+// var a=1;
+// var b=2;
 
-console.log("sum of 1 and 2 "+a+b)   
+// console.log("sum of 1 and 2 "+a+b)   
 // If we do + of string and number it will covert to string
+
+
+//Swapping  
+//1. type
+
+// let a=2;
+// let b=3;
+
+// let c= a;  //c=2, a=2 , b=3
+// a=b;   //a=3, b=3
+// b=c;  //a=3, b=2
+
+//2. type
+
+// a=a+b;  //a=5
+// b=a-b ; //b=5-3=2
+// a=a-b; //a=5-2=3
+
+//3. type
+// [a,b] = [b,a]  //destructuring
+// console.log(`a: ${a}, b : ${b}`)
+
