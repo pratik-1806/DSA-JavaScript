7f85fced2f88a763b5c6f683bda4bff63f64ab62
pratik-1806 <ppratikpansare98@gmail.com>
2025-07-26 16:34:30 +0530

    sum of natural numbers

--- Part1/conditional.js
+++ Part1/conditional.js
@@ -8,3 +8,12 @@
 // for(i=1; i<10; i++){
 //     console.log(i)
 // }
+
+//Sum of natural numbers
+
+// let num = Number(prompt("Enter number"))
+// let sum =0;
+// for(let i=1; i<=num; i++){
+//         sum =sum+i;
+// }
+// console.log(`sum of ${num} natural number is ${sum}`)
